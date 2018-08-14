ce71466feeeb063f5576b06bf67a9d2e94fdfe58
Ronnie Villarini <Villarini91@gmail.com>
2018-08-13 22:45:41 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button = document.getElementById("randomColor");
+
+setRandomColor();
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function getRandomColor() {
+	var randomColor = "";
+	var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+	return randomColor;
+}
+
+function setRandomColor() {
+	color1.value = getRandomColor();
+	color2.value = getRandomColor();
+	setGradient();
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", setRandomColor);
